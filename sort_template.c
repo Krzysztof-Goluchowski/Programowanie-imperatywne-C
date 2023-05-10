d54c1bcc900724906b593b2e0b5503749591c34f
Krzysztof Gołuchowski <131868100+Krzysztof-Goluchowski@users.noreply.github.com>
2023-05-10 17:55:16 +0200

    Update sort_template.c

--- sort_template.c
+++ sort_template.c
@@ -4,9 +4,9 @@
 #include <time.h>
 
 typedef struct {
-	int day; 
-	int month; 
-	int year;
+    int day;
+    int month;
+    int year;
 } Date;
 
 // 1 bsearch2
@@ -16,10 +16,10 @@ typedef struct {
 #define RECORD_MAX 40  // max. line length
 
 typedef struct {
-	char name[20];
-	float price;
-	int amount;
-	Date valid_date;
+    char name[20];
+    float price;
+    int amount;
+    Date valid_date;
 } Food;
 
 typedef int (*ComparFp)(const void *, const void *);
@@ -40,20 +40,29 @@ int cmp(const void *a, const void *b) {
     const Food* food1 = (const Food*)a;
     const Food* food2 = (const Food*)b;
 
-    Date data1 = food1->valid_date;
-    Date data2 = food2->valid_date;
-
-    int roznica_dni = cmp_date(&data1, &data2);
-    if (roznica_dni > 0){
-        return 1;
-    } else if (roznica_dni < 0){
-        return -1;
-    }
 
     if (food1->price < food2->price) {
-        return -1;
+        return -3;
     } else if (food1->price > food2->price) {
-        return 1;
+        return 3;
+    }
+
+    if (food1->valid_date.year < food2->valid_date.year){
+        return -2;
+    } else if (food1->valid_date.year > food2->valid_date.year){
+        return 2;
+    } else {
+        if (food1->valid_date.month < food2->valid_date.month){
+            return -2;
+        } else if (food1->valid_date.month > food2->valid_date.month){
+            return 2;
+        } else {
+            if (food1->valid_date.day < food2->valid_date.day){
+                return -2;
+            } else if (food1->valid_date.day > food2->valid_date.day){
+                return 2;
+            }
+        }
     }
 
     return strcmp(food1->name, food2->name);
@@ -61,7 +70,20 @@ int cmp(const void *a, const void *b) {
 
 // bsearch returning address where to insert new element
 void *bsearch2 (const void *key, const void *base, size_t nmemb, size_t size, ComparFp compar, char *result) {
-    
+    void *baza = base;
+    for (int i = 0; i < nmemb; ++i) {
+        int compare = compar(&key, &base[i]);
+        if (compare == 0){
+            *result = 1;
+            return &baza[i];
+        } else if (compare == -1 || compare == -2 || compare == -3){
+            *result = 0;
+            return &baza[i];
+        } else if (compare == 2 || compare == 3 || compare == 1){
+            continue;
+        }
+
+    }
 }
 
 // print goods of given name
@@ -76,7 +98,7 @@ void print_art(Food *p, int n, char *art) {
 // add record to table if absent
 Food* add_record(Food *tab, int *np, ComparFp compar, Food *new) {
     char found;
-    Food *food = bsearch2(new, tab, *np, sizeof(Food), compar, &found);
+    Food *food = bsearch2(&new, tab, *np, sizeof(Food), compar, &found);
     if (found){
         food->amount += new->amount;
     } else {
@@ -105,14 +127,14 @@ int read_goods(Food *tab, int no, FILE *stream, int sorted) {
 }
 
 int cmp_qs(const void *a, const void *b) {
-	Food *fa = (Food*)a, *fb = (Food*)b;
-	return cmp_date(&fa->valid_date, &fb->valid_date);
+    Food *fa = (Food*)a, *fb = (Food*)b;
+    return cmp_date(&fa->valid_date, &fb->valid_date);
 }
 
 int cmp_bs(const void *a, const void *b) {
-	Date *pd = (Date*)a;
-	Food *fb = (Food*)b;
-	return cmp_date(pd, &fb->valid_date);
+    Date *pd = (Date*)a;
+    Food *fb = (Food*)b;
+    return cmp_date(pd, &fb->valid_date);
 }
 
 // finds the value of goods due on 'curr_date'
@@ -128,29 +150,29 @@ enum Sex {F, M};
 enum BOOL {no, yes};
 
 struct Bit_data {
-	enum Sex sex:1;
-	enum BOOL in_line:1;
+    enum Sex sex:1;
+    enum BOOL in_line:1;
 };
 
 typedef struct {
-	char *name;
-	struct Bit_data bits;
-	Date born;
-	char *parent;
+    char *name;
+    struct Bit_data bits;
+    Date born;
+    char *parent;
 } Person;
 
 typedef struct {
-	char *par_name;
-	int index;
+    char *par_name;
+    int index;
 } Parent;
 
 const Date primo_date = { 28, 10, 2011 }; // new succession act
 
 int cmp_primo(Person *person1, Person *person2) {
-	if (person1->bits.sex == person2->bits.sex) return 0;
-	if (person1->bits.sex == F && cmp_date(&person1->born, &primo_date) > 0) return 0;
-	if (person2->bits.sex == F && cmp_date(&person2->born, &primo_date) > 0) return 0;
-	return person2->bits.sex - person1->bits.sex;
+    if (person1->bits.sex == person2->bits.sex) return 0;
+    if (person1->bits.sex == F && cmp_date(&person1->born, &primo_date) > 0) return 0;
+    if (person2->bits.sex == F && cmp_date(&person2->born, &primo_date) > 0) return 0;
+    return person2->bits.sex - person1->bits.sex;
 }
 
 // compare persons
@@ -167,82 +189,82 @@ int cleaning(Person *person_tab, int n) {
 }
 
 void print_person(const Person *p) {
-	printf("%s\n", p->name);
+    printf("%s\n", p->name);
 }
 
 int create_list(Person *person_tab, int n) {
 }
 
 int main(void) {
-	Person person_tab[] = {
-		{"Charles III", {M, no}, {14, 11, 1948},"Elizabeth II"},
-		{"Anne", {F,yes}, {15, 8, 1950},"Elizabeth II"},
-		{"Andrew", {M,yes}, {19, 2, 1960},"Elizabeth II"},
-		{"Edward", {M,yes}, {10, 3, 1964} ,"Elizabeth II"},
-		{"David", {M,yes}, {3, 11, 1961} ,"Margaret"},
-		{"Sarah", {F,yes}, {1, 5, 1964}, "Margaret"},
-		{"William", {M,yes}, {21, 6, 1982}, "Charles III"},
-		{"Henry", {M,yes}, {15, 9, 1984}, "Charles III"},
-		{"Peter", {M,yes}, {15, 11, 1977}, "Anne"},
-		{"Zara", {F,yes}, {15, 5, 1981}, "Anne"},
-		{"Beatrice", {F,yes}, {8, 8, 1988}, "Andrew"},
-		{"Eugenie", {F,yes}, {23, 3, 1990}, "Andrew"},
-		{"James", {M,yes}, {17, 12, 2007}, "Edward"},
-		{"Louise", {F,yes}, {8, 11, 2003}, "Edward"},
-		{"Charles", {M,yes}, {1, 7, 1999}, "David"},
-		{"Margarita", {F,yes}, {14, 5, 2002}, "David"},
-		{"Samuel", {M,yes}, {28, 7, 1996}, "Sarah"},
-		{"Arthur", {M,yes}, {6, 5, 2019}, "Sarah"},
-		{"George", {M,yes}, {22, 7, 2013}, "William"},
-		{"George VI", {M,no}, {14, 12, 1895}, NULL},
-		{"Charlotte", {F,yes}, {2, 5, 2015}, "William"},
-		{"Louis", {M,yes}, {23, 4, 2018}, "William"},
-		{"Archie", {M,yes}, {6, 5, 2019}, "Henry"},
-		{"Lilibet", {F,yes}, {4, 6, 2021}, "Henry"},
-		{"Savannah", {F,yes}, {29, 12, 2010}, "Peter"},
-		{"Isla", {F,yes}, {29, 3, 2012}, "Peter"},
-		{"Mia", {F,yes}, {17, 1, 2014}, "Zara"},
-		{"Lena", {F,yes}, {18, 6, 2018}, "Zara"},
-		{"Elizabeth II", {F,no}, {21, 4, 1925}, "George VI"},
-		{"Margaret", {F,no}, {21, 8, 1930}, "George VI"},
-		{"Lucas", {M,yes}, {21, 3, 2021}, "Zara"},
-		{"Sienna", {F,yes}, {18, 9, 2021}, "Beatrice"},
-		{"August", {M,yes}, {9, 2, 2021}, "Eugenie"}
-	};
+    Person person_tab[] = {
+            {"Charles III", {M, no}, {14, 11, 1948},"Elizabeth II"},
+            {"Anne", {F,yes}, {15, 8, 1950},"Elizabeth II"},
+            {"Andrew", {M,yes}, {19, 2, 1960},"Elizabeth II"},
+            {"Edward", {M,yes}, {10, 3, 1964} ,"Elizabeth II"},
+            {"David", {M,yes}, {3, 11, 1961} ,"Margaret"},
+            {"Sarah", {F,yes}, {1, 5, 1964}, "Margaret"},
+            {"William", {M,yes}, {21, 6, 1982}, "Charles III"},
+            {"Henry", {M,yes}, {15, 9, 1984}, "Charles III"},
+            {"Peter", {M,yes}, {15, 11, 1977}, "Anne"},
+            {"Zara", {F,yes}, {15, 5, 1981}, "Anne"},
+            {"Beatrice", {F,yes}, {8, 8, 1988}, "Andrew"},
+            {"Eugenie", {F,yes}, {23, 3, 1990}, "Andrew"},
+            {"James", {M,yes}, {17, 12, 2007}, "Edward"},
+            {"Louise", {F,yes}, {8, 11, 2003}, "Edward"},
+            {"Charles", {M,yes}, {1, 7, 1999}, "David"},
+            {"Margarita", {F,yes}, {14, 5, 2002}, "David"},
+            {"Samuel", {M,yes}, {28, 7, 1996}, "Sarah"},
+            {"Arthur", {M,yes}, {6, 5, 2019}, "Sarah"},
+            {"George", {M,yes}, {22, 7, 2013}, "William"},
+            {"George VI", {M,no}, {14, 12, 1895}, NULL},
+            {"Charlotte", {F,yes}, {2, 5, 2015}, "William"},
+            {"Louis", {M,yes}, {23, 4, 2018}, "William"},
+            {"Archie", {M,yes}, {6, 5, 2019}, "Henry"},
+            {"Lilibet", {F,yes}, {4, 6, 2021}, "Henry"},
+            {"Savannah", {F,yes}, {29, 12, 2010}, "Peter"},
+            {"Isla", {F,yes}, {29, 3, 2012}, "Peter"},
+            {"Mia", {F,yes}, {17, 1, 2014}, "Zara"},
+            {"Lena", {F,yes}, {18, 6, 2018}, "Zara"},
+            {"Elizabeth II", {F,no}, {21, 4, 1925}, "George VI"},
+            {"Margaret", {F,no}, {21, 8, 1930}, "George VI"},
+            {"Lucas", {M,yes}, {21, 3, 2021}, "Zara"},
+            {"Sienna", {F,yes}, {18, 9, 2021}, "Beatrice"},
+            {"August", {M,yes}, {9, 2, 2021}, "Eugenie"}
+    };
 
-	int to_do, no;
-	int n;
-	Food food_tab[FOOD_MAX];
-	char buff[ART_MAX];
-	fgets(buff, ART_MAX, stdin);
-	sscanf(buff, "%d", &to_do);
+    int to_do, no;
+    int n;
+    Food food_tab[FOOD_MAX];
+    char buff[ART_MAX];
+    fgets(buff, ART_MAX, stdin);
+    sscanf(buff, "%d", &to_do);
 
-	switch (to_do) {
-		case 1:  // bsearch2
-			fgets(buff, ART_MAX, stdin);
-			sscanf(buff, "%d", &no);
-			n = read_goods(food_tab, no, stdin, 1);
-			scanf("%s",buff);
-			print_art(food_tab, n, buff);
-			break;
-		case 2: // qsort
-			fgets(buff, ART_MAX, stdin);
-			sscanf(buff, "%d", &no);
-			n = read_goods(food_tab, no, stdin, 0);
-			Date curr_date;
-			int days;
-			scanf("%d %d %d", &curr_date.day, &curr_date.month, &curr_date.year);
-			scanf("%d", &days);
-			printf("%.2f\n", value(food_tab, (size_t)n, curr_date, days));
-			break;
-		case 3: // succession
-			scanf("%d",&no);
-			int no_persons = sizeof(person_tab) / sizeof(Person);
-			create_list(person_tab,no_persons);
-			print_person(person_tab + no - 1);
-			break;
-		default:
-			printf("NOTHING TO DO FOR %d\n", to_do);
-	}
-	return 0;
+    switch (to_do) {
+        case 1:  // bsearch2
+            fgets(buff, ART_MAX, stdin);
+            sscanf(buff, "%d", &no);
+            n = read_goods(food_tab, no, stdin, 1);
+            scanf("%s",buff);
+            print_art(food_tab, n, buff);
+            break;
+        case 2: // qsort
+            fgets(buff, ART_MAX, stdin);
+            sscanf(buff, "%d", &no);
+            n = read_goods(food_tab, no, stdin, 0);
+            Date curr_date;
+            int days;
+            scanf("%d %d %d", &curr_date.day, &curr_date.month, &curr_date.year);
+            scanf("%d", &days);
+            printf("%.2f\n", value(food_tab, (size_t)n, curr_date, days));
+            break;
+        case 3: // succession
+            scanf("%d",&no);
+            int no_persons = sizeof(person_tab) / sizeof(Person);
+            create_list(person_tab,no_persons);
+            print_person(person_tab + no - 1);
+            break;
+        default:
+            printf("NOTHING TO DO FOR %d\n", to_do);
+    }
+    return 0;
 }
